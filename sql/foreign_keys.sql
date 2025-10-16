39f5cc535bc837b650170049d26647d60411cfd5
ivan-zamurenko <ivan.zamurenko.ds@gmail.com>
2025-10-16 13:11:03 +0100

    Add Foreign Key for ORDERS
    Add foreign key ORDERS(channel_id) -> CHANNELS(channel_id)

--- sql/foreign_keys.sql
+++ sql/foreign_keys.sql
@@ -7,8 +7,19 @@
     From orders o
     Where store_id Not In (Select store_id From stores s);
 
-
     --?> Create a foreign key for orders(store_id) -> stores(store_id)
     Alter Table orders
     Add Constraint fk_orders_stores
     Foreign Key (store_id) References stores(store_id);
+
+
+    --?> 1.2 Check for non-matching channel_id in orders table
+    Select o.channel_id
+    From orders o
+    Left Join channels c On c.channel_id = o.channel_id
+    Where c.channel_id Is Null;
+
+    --?> Create a foreign key for orders(channel_id) -> channels(channel_id)
+    Alter Table orders
+    Add constraint fk_orders_channels
+    Foreign Key (channel_id) References channels(channel_id);
